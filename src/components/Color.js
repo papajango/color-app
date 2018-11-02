62f8c63f82cb4ba7ffae25d40aa3fbc46cd96878
Dmitriy Kondakov <diman4life@mail.ru>
2018-11-02 14:41:23 +0300

    remove Component import

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import StarRating from './StarRating';
 import PropTypes from 'prop-types';
 import '../styles/Color.scss';
--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Color from './Color';
 import '../styles/ColorList.scss';
 import PropTypes from 'prop-types';
